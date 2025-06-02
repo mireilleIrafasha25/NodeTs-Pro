3933768ab05bc555a1237bc8a34d142644d68e5e
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-06-02 06:56:13 +0200

    dh

--- src/routes/blogRoute.ts
+++ src/routes/blogRoute.ts
@@ -1,12 +1,14 @@
 
 import express ,{Router} from "express";
-import {AddBlog} from "../controller/blogController"
+import {AddBlog,GetBlog,GetBlogById,UpdateBlog,DeleteBlog} from "../controller/blogController"
 import {validate} from "../middleware/validation.middleware"
 import {addBlogSchema} from "../schemas/blogSchemas"
 import upload from "../middleware/multer"
 import {authenticateToken,authorize} from "../middleware/authenthicateToken"
 const route:Router=express.Router();
-
 route.post('/addBlog',authenticateToken,authorize("admin"),upload.single('image'),validate(addBlogSchema),AddBlog);
-
+route.get("/listBlog",GetBlog)
+route.get("/getBlogById/:id",GetBlogById)
+route.put("/updateBlog/:id",UpdateBlog)
+route.delete("/deleteBlog/:id",DeleteBlog)
 export default route;
